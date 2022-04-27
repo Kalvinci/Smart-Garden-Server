3ddb97555da2bf1d6e4fcce1e858eb700333680e
Akash <akash7nair@gmail.com>
2022-04-26 20:31:05 -0400

    increased resolution

--- checkPlant.sh
+++ checkPlant.sh
@@ -1,4 +1,5 @@
 cd /home/akash/Documents/repos/Smart-Garden-Server
 # cd /home/gman/Documents/garden/Smart-Garden-Server
-sudo fswebcam -r 640x480 --jpeg 85 -D 1 /home/akash/Documents/repos/Smart-Garden-Server/public/images/plant.jpg --device /dev/video2
+#sudo fswebcam -r 640x480 --jpeg 85 -D 1 /home/akash/Documents/repos/Smart-Garden-Server/public/images/plant.jpg --device /dev/video2
+sudo fswebcam -r 1080x720 --jpeg 85 -D 1 /home/akash/Documents/repos/Smart-Garden-Server/public/images/plant.jpg --device /dev/video2
 # sudo cp plant.jpg /home/akash/Documents/repos/Smart-Garden-Server/public/images
